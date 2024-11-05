a891606df6fae0ab03267a2a81d4a720b873d89e
shrashti-19 <shrashtidwivedi91@gmail.com>
2024-11-05 21:27:00 +0530

    Files modified

---
+++ src/constants.js
@@ -0,0 +1,1 @@
+export const DB_NAME ="shrashti"//application ke database ka name
---
+++ src/db/index.js
@@ -0,0 +1,20 @@
+import mongoose from 'mongoose';
+import {DB_NAME} from "../constants.js";
+
+const connectDB = async()=>{
+    try{
+        const connectionInstance = 
+        await mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`)
+        console.log(`\n MongoDb connected !! DB HOST : ${connectionInstance}`);
+        console.log(connectionInstance);
+        
+        
+    }catch(error){
+        console.log("MongoDB connection error",error);
+        process.exit(1);
+
+        
+    }
+}
+
+export default connectDB;
